8d5325fffd8a80ba8466e341e5e20d7fac399904
Alissa Cohan <alissacohan18@gmail.com>
2023-01-26 19:24:22 -0500

    created api routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const fs = require("fs");
+const path = require("path");
+
+const dbNotes = require("../../db/db.json");
+
+router.get("/api/notes", (req, res) => {
+  res.json(dbNotes);
+});
+
+router.post("/api/notes", (req, res) => {
+  const notes = createNote(req.body);
+  res.json(notes);
+});
+
+function createNote(body) {
+  dbNotes.push(body);
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify({ dbNotes }, null, 2)
+  );
+  return body;
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//not including a file name defaults to the 'index' file in these directories
+const apiRoutes = require('./routes/apiRoutes');
+//const htmlRoutes = require('./routes/htmlRoutes');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+//middleware instructs the server to make these (style) files static resources
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use(apiRoutes);
+//app.use('/', htmlRoutes);
+
+
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
